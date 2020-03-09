0f4ac33941c61a8355613f42b23532ee5e7b6a79
Thapelo Kekana <thapelosimon@gmail.com>
2020-03-09 02:56:28 +0200

    Updates

---
+++ precache-manifest.821165d6c66dcddfa0b224a95945a6d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cd0e598dde66357989cc103c34515a",
+    "url": "/Thapelo1/league-digital-tech-assessment/index.html"
+  },
+  {
+    "revision": "c22a2f9138dbd247a471",
+    "url": "/Thapelo1/league-digital-tech-assessment/static/css/main.15cc8aa6.chunk.css"
+  },
+  {
+    "revision": "099402aad07ea12db410",
+    "url": "/Thapelo1/league-digital-tech-assessment/static/js/2.fb39de4a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Thapelo1/league-digital-tech-assessment/static/js/2.fb39de4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22a2f9138dbd247a471",
+    "url": "/Thapelo1/league-digital-tech-assessment/static/js/main.53109c6e.chunk.js"
+  },
+  {
+    "revision": "4bec8529962d3886a0e9",
+    "url": "/Thapelo1/league-digital-tech-assessment/static/js/runtime-main.acac8d36.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Thapelo1/league-digital-tech-assessment/precache-manifest.821165d6c66dcddfa0b224a95945a6d3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Thapelo1/league-digital-tech-assessment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
